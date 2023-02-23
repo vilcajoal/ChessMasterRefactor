f9729111b81394f608535b1d3fc5625058d015cd
vilcajoal <49346518+vilcajoal@users.noreply.github.com>
2023-02-22 21:32:03 -0500

    Singleton a Clase ChessGraveyard.java

--- Original.src/ChessGraveyard.java
+++ Original.src/ChessGraveyard.java
@@ -1,48 +1,32 @@
 import java.awt.GridLayout;
 import javax.swing.*;
-// -------------------------------------------------------------------------
-/**
- * Represents the graveyard where pieces lie after being captured.
- *
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
-public class ChessGraveyard
-    extends JPanel{
-    private String title;
-    // ----------------------------------------------------------
-    /**
-     * Create a new ChessGraveyard object.
-     *
-     * @param title
-     *            the title of the graveyard
-     */
-    public ChessGraveyard( String title ){
-        this.title = title;
-        this.add( new JLabel( title ) );
-        this.setLayout( new GridLayout( 8, 0 ) );
+
+public class ChessGraveyard extends JPanel {
+    private static ChessGraveyard instance;
+    private String graveyardTitle;
+
+    private ChessGraveyard(String graveyardTitle) {
+        this.graveyardTitle = graveyardTitle;
+        this.add(new JLabel(graveyardTitle));
+        this.setLayout(new GridLayout(8, 0));
     }
-    // ----------------------------------------------------------
-    /**
-     * Adds a piece to the graveyard.
-     *
-     * @param piece
-     *            the GamePiece to add
-     */
-    public void addPiece( ChessGamePiece piece ){
-        piece.setPieceLocation( -1, -1 );
+
+    public static ChessGraveyard getInstance(String graveyardTitle) {
+        if (instance == null) {
+            instance = new ChessGraveyard(graveyardTitle);
+        }
+        return instance;
+    }
+
+    public void addPiece(ChessPiece piece) {
+        piece.setPieceLocation(-1, -1);
         JLabel pieceLabel = new JLabel();
-        pieceLabel.setIcon( piece.getImage() );
-        this.add( pieceLabel );
+        pieceLabel.setIcon(piece.getImage());
+        this.add(pieceLabel);
     }
-    // ----------------------------------------------------------
-    /**
-     * Removes all pieces from the graveyard.
-     */
-    public void clearGraveyard(){
+
+    public void clearGraveyard() {
         this.removeAll();
-        this.add( new JLabel( title ) );
+        this.add(new JLabel(graveyardTitle));
     }
 }
