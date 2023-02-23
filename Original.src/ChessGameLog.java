de147bede5d64d5ea0ca31adfcf911e9af8db6d8
vilcajoal <49346518+vilcajoal@users.noreply.github.com>
2023-02-22 21:28:00 -0500

    Singleton a Clase ChessGameLog.java

--- Original.src/ChessGameLog.java
+++ Original.src/ChessGameLog.java
@@ -1,64 +1,39 @@
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import java.util.Date;
-// -------------------------------------------------------------------------
-/**
- * A scrollable textArea representing the game log. (i.e. moves made by each
- * player)
- * 
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
-public class ChessGameLog
-    extends JScrollPane{
-    private JTextArea textArea;
-    // ----------------------------------------------------------
-    /**
-     * Create a new ChessGameLog object.
-     */
-    public ChessGameLog(){
-        super(
-            new JTextArea( "", 5, 30 ),
-            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
-        textArea = ( (JTextArea)this.getViewport().getView() );
+
+public class ChessGameLog extends JScrollPane {
+private static ChessGameLog instance = null;
+private JTextArea textArea;
+private ChessGameLog() {
+    super(new JTextArea("", 5, 30), JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    textArea = ((JTextArea) this.getViewport().getView());
+}
+
+public static ChessGameLog getInstance() {
+    if (instance == null) {
+        instance = new ChessGameLog();
     }
-    // ----------------------------------------------------------
-    /**
-     * Adds a new line of text to the log.
-     * 
-     * @param s
-     *            the line of text to add
-     */
-    public void addToLog( String s ){
-        if ( textArea.getText().length() > 0 ){
-            textArea.setText( textArea.getText() + "\n" + new Date() + " - "
-                + s );
-        }
-        else
-        {
-            textArea.setText( new Date() + " - " + s );
-        }
+    return instance;
+}
+
+public void addToLog(String s) {
+    if (textArea.getText().length() > 0) {
+        textArea.setText(textArea.getText() + "\n" + new Date() + " - " + s);
+    } else {
+        textArea.setText(new Date() + " - " + s);
     }
-    /**
-     * Clears the log.
-     */
-    public void clearLog(){
-        textArea.setText( "" );
+}
+
+public void clearLog() {
+    textArea.setText("");
+}
+
+public String getLastLog() {
+    int indexOfLastNewLine = textArea.getText().lastIndexOf("\n");
+    if (indexOfLastNewLine < 0) {
+        return textArea.getText();
     }
-    // ----------------------------------------------------------
-    /**
-     * Gets the most recent statement added to the log.
-     * 
-     * @return String the most recent log statement
-     */
-    public String getLastLog(){
-        int indexOfLastNewLine = textArea.getText().lastIndexOf( "\n" );
-        if ( indexOfLastNewLine < 0 ){
-            return textArea.getText();
-        }
-        return textArea.getText().substring( indexOfLastNewLine + 1 );
-    }
+    return textArea.getText().substring(indexOfLastNewLine + 1);
+}
 }
