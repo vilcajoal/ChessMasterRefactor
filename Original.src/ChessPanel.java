1d53ba67cecca00a7cea67d6e62788a31297aeef
vilcajoal <49346518+vilcajoal@users.noreply.github.com>
2023-03-03 22:46:38 -0500

    Patron Prototype

--- Original.src/ChessPanel.java
+++ Original.src/ChessPanel.java
@@ -1,21 +1,30 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
 public class ChessPanel extends JPanel implements Cloneable {
+private static final int PLAYER_ONE = 1;
+private static final int PLAYER_TWO = 2;
+
 private ChessMenuBar menuBar;
 private ChessGameBoard gameBoard;
 private ChessGameLog gameLog;
 private ChessGraveyard playerOneGraveyard;
 private ChessGraveyard playerTwoGraveyard;
 private ChessGameEngine gameEngine;
-public ChessPanel() {
+
+public ChessPanel(ChessMenuBar menuBar, ChessGameBoard gameBoard, ChessGameLog gameLog, ChessGraveyard playerOneGraveyard, ChessGraveyard playerTwoGraveyard) {
+    this.menuBar = menuBar;
+    this.gameBoard = gameBoard;
+    this.gameLog = gameLog;
+    this.playerOneGraveyard = playerOneGraveyard;
+    this.playerTwoGraveyard = playerTwoGraveyard;
+    initialize();
+}
+
+private void initialize() {
     this.setLayout(new BorderLayout());
-    menuBar = new ChessMenuBar();
-    gameBoard = new ChessGameBoard();
-    gameLog = new ChessGameLog();
-    playerOneGraveyard = new ChessGraveyard("Player 1's graveyard");
-    playerTwoGraveyard = new ChessGraveyard("Player 2's graveyard");
     this.add(menuBar, BorderLayout.NORTH);
     this.add(gameBoard, BorderLayout.CENTER);
     this.add(gameLog, BorderLayout.SOUTH);
@@ -26,19 +35,7 @@ public ChessPanel() {
 }
 
 public ChessPanel(ChessPanel panel) {
-    this.setLayout(new BorderLayout());
-    this.menuBar = (ChessMenuBar) panel.menuBar.clone();
-    this.gameBoard = (ChessGameBoard) panel.gameBoard.clone();
-    this.gameLog = (ChessGameLog) panel.gameLog.clone();
-    this.playerOneGraveyard = (ChessGraveyard) panel.playerOneGraveyard.clone();
-    this.playerTwoGraveyard = (ChessGraveyard) panel.playerTwoGraveyard.clone();
-    this.add(menuBar, BorderLayout.NORTH);
-    this.add(gameBoard, BorderLayout.CENTER);
-    this.add(gameLog, BorderLayout.SOUTH);
-    this.add(playerOneGraveyard, BorderLayout.WEST);
-    this.add(playerTwoGraveyard, BorderLayout.EAST);
-    this.setPreferredSize(new Dimension(800, 600));
-    gameEngine = new ChessGameEngine(gameBoard);
+    this(panel.menuBar, panel.gameBoard, panel.gameLog, panel.playerOneGraveyard, panel.playerTwoGraveyard);
 }
 
 public ChessMenuBar getMenuBar() {
@@ -54,21 +51,5 @@ public ChessGameLog getGameLog() {
 }
 
 public ChessGraveyard getGraveyard(int whichPlayer) {
-    if (whichPlayer == 1) {
-        return playerOneGraveyard;
-    } else if (whichPlayer == 2) {
-        return playerTwoGraveyard;
-    } else {
-        return null;
-    }
-}
-
-public ChessGameEngine getGameEngine() {
-    return gameEngine;
-}
-
-@Override
-protected Object clone() throws CloneNotSupportedException {
-    return new ChessPanel(this);
-}
-}
+    switch (whichPlayer) {
+        case PLAYER_ONE
