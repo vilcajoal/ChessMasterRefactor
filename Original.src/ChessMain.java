efa969930c490f371c2dd033f9bd0231efe3f498
vilcajoal <49346518+vilcajoal@users.noreply.github.com>
2023-03-04 08:03:19 -0500

    PatronAdapter

--- Original.src/ChessMain.java
+++ Original.src/ChessMain.java
@@ -1,26 +1,18 @@
 import javax.swing.*;
-// -------------------------------------------------------------------------
-/**
- * Shows the GUI for the Chess game.
- * 
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
-public class ChessMain{
-    // ----------------------------------------------------------
-    /**
-     * Creates the GUI for Chess.
-     * 
-     * @param args
-     *            command line arguments, not used
-     */
-    public static void main( String[] args ){
-        JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.getContentPane().add( new ChessPanel() );
+
+public class ChessMain {
+
+    public static void main(String[] args) {
+        JFrame frame = createFrame();
+        frame.setVisible(true);
+    }
+
+    private static JFrame createFrame() {
+        JFrame frame = new JFrame("YetAnotherChessGame 1.0");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new ChessPanel());
         frame.pack();
-        frame.setVisible( true );
+        return frame;
     }
 }
+
